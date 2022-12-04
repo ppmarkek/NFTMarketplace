1f6cc6e5c55686dde4d07d9de16ea9257e7a9805
markek <markungeitis01@gmail.com>
2022-12-04 19:29:52 +0200

    feat: add react, add header

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import NavigationBar from './components/templates/NavigationBar/NavigationBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavigationBar />
+    </div>
+  );
+}
+
+export default App;
